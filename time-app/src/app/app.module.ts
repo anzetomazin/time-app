29617d04bc95fcf797f796ef744391e4c5ad860e
Anzx <31044008+anzetomazin@users.noreply.github.com>
2020-06-17 21:59:48 +0200

    feat: add material imports to modules

--- time-app/src/app/app.module.ts
+++ time-app/src/app/app.module.ts
@@ -6,26 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon'; 
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'; 
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    MatListModule,
-    MatButtonModule,
-    MatGridListModule,
-    MatIconModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatSidenavModule,
+		MatListModule,
+		MatButtonModule,
+		MatIconModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- time-app/src/app/employees/employees.module.ts
+++ time-app/src/app/employees/employees.module.ts
@@ -6,12 +6,28 @@ import { PresentListComponent } from './present-list/present-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeAddComponent } from './employee-add/employee-add.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+
 
 @NgModule({
-  declarations: [EmployeeListComponent, EmployeeAddComponent, PresentListComponent, EmployeeListComponent, EmployeeAddComponent],
-  imports: [
-    CommonModule,
-    EmployeesRoutingModule
-  ]
+	declarations: [EmployeeListComponent, EmployeeAddComponent, PresentListComponent],
+	imports: [
+		CommonModule,
+		EmployeesRoutingModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		MatIconModule,
+		MatListModule,
+	]
 })
 export class EmployeesModule { }
