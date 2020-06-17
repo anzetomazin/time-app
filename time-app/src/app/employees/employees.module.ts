4e550a460a7511b1b6de144115c197886e3813dc
Anzx <31044008+anzetomazin@users.noreply.github.com>
2020-06-17 12:05:28 +0200

    feat: add employee & presence components to employee module

--- time-app/src/app/employees/employees.module.ts
+++ time-app/src/app/employees/employees.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { PresentListComponent } from './present-list/present-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserListComponent, UserAddComponent, PresentListComponent, EmployeeListComponent, EmployeeAddComponent],
   imports: [
     CommonModule,
     EmployeesRoutingModule
---
+++ time-app/src/app/employees/present-list/present-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-present-list',
+  templateUrl: './present-list.component.html',
+  styleUrls: ['./present-list.component.css']
+})
+export class PresentListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
