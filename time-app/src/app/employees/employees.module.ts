3c5bd463793393bc4edd7e23032d66cdd4092d54
Anzx <31044008+anzetomazin@users.noreply.github.com>
2020-06-17 14:40:35 +0200

    feat: create employee model and service

--- time-app/src/app/employees/employees.module.ts
+++ time-app/src/app/employees/employees.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserAddComponent } from './user-add/user-add.component';
 import { PresentListComponent } from './present-list/present-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeAddComponent } from './employee-add/employee-add.component';
 
 
 @NgModule({
-  declarations: [UserListComponent, UserAddComponent, PresentListComponent, EmployeeListComponent, EmployeeAddComponent],
+  declarations: [EmployeeListComponent, EmployeeAddComponent, PresentListComponent, EmployeeListComponent, EmployeeAddComponent],
   imports: [
     CommonModule,
     EmployeesRoutingModule
