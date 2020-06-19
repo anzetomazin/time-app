08dc7e3e05c522e4f9fb069c5a8dfd349206dcae
Anzx <31044008+anzetomazin@users.noreply.github.com>
2020-06-19 14:36:09 +0200

    feat: add api methods

--- time-app/src/app/core/api.service.ts
+++ time-app/src/app/core/api.service.ts
@@ -3,35 +3,38 @@ import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http'
 import { Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { catchError } from 'rxjs/operators';
+import { SnackbarService } from './snackbar.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+  ) { 
+  }
 
   get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
     return this.http.get(`${environment.baseUrl}${path}`, { params })
                     .pipe(catchError(this.handleError));
   }
 
+  put(path: string, body: object = {}): Observable<any> {
+    return this.http.put(`${environment.baseUrl}${path}`, JSON.stringify(body))
+                    .pipe(catchError(this.handleError));
+  }
+
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
       console.error(
         `Backend returned code ${error.status}, ` +
         `body was: ${error.error}`);
     }
-    // return an observable with a user-facing error message
     return throwError(
       'Something bad happened; please try again later.');
   };
-  
+
 }
--- time-app/src/app/employees/shared/employee.service.ts
+++ time-app/src/app/employees/shared/employee.service.ts
@@ -3,18 +3,30 @@ import { ApiService } from 'src/app/core/api.service';
 import { Observable } from 'rxjs';
 import { Employee } from './employee.model';
 import { map } from 'rxjs/operators';
+import { HttpParams } from '@angular/common/http';
+import { Timestamp } from 'rxjs/internal/operators/timestamp';
 
 @Injectable({
-  providedIn: 'root'
+providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor(
-    private apiService : ApiService
-  ) { }
+	constructor(
+		private apiService : ApiService
+	) { }
 
-  getAll(): Observable<Employee[]> {
-    return this.apiService.get(`/employee`)
-        .pipe(map(data => data));
-  }
+	getAllPresent(): Observable<Employee[]> {
+		return this.apiService.get(`/Presence?TimeStamp=${new Date().toJSON()}&OrgUnitID=10000083&showInactiveEmployees=false`)
+			.pipe(map(data => data));
+	}
+
+	getAll(): Observable<Employee[]> {
+		return this.apiService.get(`/employee`)
+			.pipe(map(data => data));
+	}
+
+	put(employee: Employee): Observable<Employee> {
+		return this.apiService.put(`/employee`, employee)
+			.pipe(map(data => data));
+	}
 }
